0f402979a1aeeb0eb5feff877a71678c7292a09e
Yu-Ju Chen <casbie23@gmail.com>
2014-06-14 11:28:54 +0900

    refine the number of species and family

--- LODredlist.py
+++ LODredlist.py
@@ -2,16 +2,19 @@
 import csv
 
 source_list=['呂勝由','特稀圖','FOT','特稀有','賴明洲','渡假','文建會','教育廳','蘇鴻傑1','蘇鴻傑2']
+family_name=''
 
 def read_input_data(filename):
 	fp=open(filename,'r')
 	fp_out=open("Taiwan_Redlist.rdf",'w')
 	
 	for row in csv.DictReader(fp):
+
 		species_number=row['編號']
 		resource_uri='http://lod.ac/redlist/readlist-taiwan-'+species_number
 
-		science_name=row['學名']
+		science_name=row['學名'].strip()
+		science_name_label=science_name
 		temp=science_name.split(' ')
 		if temp>1:
 			science_name=''
@@ -23,14 +26,21 @@ def read_input_data(filename):
 
 		chinese_name=row['中名']
 		evaluation=row['評估']
-		#source=[]
-		#number=0
-		#for s in source_list:
-		#	source.append(row[s])
-		#	number=number+1
+		
+		if science_name.isupper(): #is family name
+			family_name=science_name.lower().capitalize()
+			sub='<http://lod.ac/species/'+family_name+'>'
+			pre='speciesOnto:hasCommonName'
+			obj='<http://lod.ac/species/'+chinese_name+'>'
+			fp_out.write(sub+' '+pre+' '+obj+'.\n')
+
+			sub=obj
+			pre='rdfs:label'
+			obj='\"'+chinese_name+'\"@zh'
+			fp_out.write(sub+' '+pre+' '+obj+'.\n')
+			continue
 		
 		descriptions=[]
-		
 		descriptions.append(('http://lod.ac/ns/cnsv#ofSpecies',science_name))
 		descriptions.append(('http://lod.ac/ns/cnsv#ofArea','台灣 @zh'))
 		if evaluation:
@@ -47,6 +57,45 @@ def read_input_data(filename):
 			else:
 				fp_out.write(' .\n')
 
+		#information of scientific name
+		sub='<http://lod.ac/species/'+science_name+'>'
+		information=[]
+		
+		pre='a'
+		obj='speciesOnto:ScientificName'
+		information.append((pre,obj))
+
+		pre='speciesOnto:hasCommonName'
+		obj='<http://lod.ac/species/'+chinese_name+'>'
+		information.append((pre,obj))
+
+		pre='speciesOnto:hasSuperTaxon'
+		obj='<http://lod.ac/species/'+family_name
+		information.append((pre,obj))
+
+		pre='rdfs:label'
+		obj='\"'+science_name_label+'\"'
+		information.append((pre,obj))
+		
+		pre='cnsvOnto:hasRedListEntry'
+		obj='redlist:readlist-taiwan-'+species_number
+		information.append((pre,obj))
+		
+		fp_out.write(sub+'\n')
+		for i in information:
+			fp_out.write('\t'+i[0]+' '+i[1])
+			if d != descriptions[-1]:
+				fp_out.write(';\n')
+			else:
+				fp_out.write(' .\n')
+
+		#fp_out.write(sub+' '+pre+' '+obj+' .\n')
+
+		#information of common name
+		sub='<http://lod.ac/species/'+chinese_name+'>'
+		pre='rdfs:label'
+		obj='\"'+chinese_name+'\"@zh'
+		fp_out.write(sub+' '+pre+' '+obj+' .\n')
 	fp.close()
 
 if __name__=='__main__':
